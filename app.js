6d474d128ad8802eeed2c7f73f93b7de49a17920
Puja-Kumari <151229965+PJCODEX@users.noreply.github.com>
2025-07-27 17:12:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-const dataPath = path.join(__dirname, 'submission.json');
+const dataPath = path.join(__dirname, 'public', 'frontend', 'submission.json');
 const submissions = [];
 
 // Load existing data if available
